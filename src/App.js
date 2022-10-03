389ca215bcaab06ec3f7fead4e3cbe5cc7eb395d
Grady Ku <gkupdx@gmail.com>
2022-10-03 16:56:27 -0700

    Update App.js with new imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import Navbar from './components/Navbar/Navbar';
+import HeroBanner from './components/HeroBanner/HeroBanner';
+import CardList from './components/CardList/CardList';
+import Footer from './components/Footer/Footer';
+
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
+      <Navbar />
+      <HeroBanner />
+      <CardList />
+      <Footer />
     </div>
   );
 }
