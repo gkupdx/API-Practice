94ebfb2408a208f43df7b91c958f6eb377e7fdde
Grady Ku <gkupdx@gmail.com>
2022-10-10 20:25:07 -0700

    Import store, reducer, and router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+/* REACT ROUTER */
+import { BrowserRouter } from 'react-router-dom';
+
+/* REACT REDUX + REDUX TOOLKIT */
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import navbarReducer from './reducers/navbar';
+
+const store = configureStore({
+  reducer: {
+    navbar: navbarReducer,
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 );
 
