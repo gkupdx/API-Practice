37b914f87470747a4162bd6d1490970aec5a9353
Grady Ku <gkupdx@gmail.com>
2022-10-10 20:27:05 -0700

    Import & render new routes, components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
+//// App.js
+
 import Navbar from './components/Navbar/Navbar';
 import HeroBanner from './components/HeroBanner/HeroBanner';
 import CardList from './components/CardList/CardList';
+import ApiList from './components/ApiList/ApiList';
+import About from './components/About/About';
 import Footer from './components/Footer/Footer';
-
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
-  
+
   return (
     <div className="App">
-      <Navbar />
-      <HeroBanner />
-      <CardList />
-      <Footer />
+      <Routes>
+        { /* HOMEPAGE route */ }
+        <Route path='/' element={
+          <>
+            <Navbar />
+            <HeroBanner />
+            <CardList />
+            <Footer />
+          </>
+        } />
+        { /* API INDEX route */ }
+        <Route path='/api-list' element={
+          <>
+            <Navbar />
+            <ApiList />
+            <Footer />
+          </>
+        } />
+        { /* ABOUT route */ }
+        <Route path='/about' element={
+          <>
+            <Navbar />
+            <About />
+            <Footer />
+          </>
+        } />
+      </Routes>
     </div>
   );
 }
