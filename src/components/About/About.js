885f582b0db1404ea2eea8e9a57729b9a4a7a60a
Grady Ku <gkupdx@gmail.com>
2022-10-25 18:23:14 -0700

    Update content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,11 +6,20 @@ const About = () => {
         <div className='about'>
             <div className='aboutMe'>
                 <h1>Hey, my name is G.Ku!</h1>
-                <p>The point of this website is to simply provide easy-to-observe
+                <p>
+                    The point of this web app is to provide easy-to-observe
                     demonstrations of some free web API. I personally struggled with
                     the concept of working with fetch API a lot, and so I wanted to
                     create a free resource for other developers to use if they're also
-                    struggling with APIs in general!
+                    struggling with APIs in general.
+                </p>
+                <p>
+                    All APIs displayed on this website <strong>do not require</strong> any
+                    API keys, personal access tokens, or authentication/authorization codes.
+                    While I understand it isn't very realistic to use auth-less APIs in a 
+                    real-world production app, because this app's target audience is for those
+                    just getting their feet wet in the API world, I wanted to minimize the overhead
+                    as much as possible for new developers.
                 </p>
             </div>
         </div>
