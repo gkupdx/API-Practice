438b5799a328afaf54f1f6b75aa73ef7cbbe103b
Grady Ku <gkupdx@gmail.com>
2022-10-25 18:24:39 -0700

    Replace Bootstrap components with custom elements

--- src/components/Docs/Docs.js
+++ src/components/Docs/Docs.js
@@ -3,32 +3,24 @@ import './docs.css';
 import optionPicker from '../../assets/undraw_option_picker.png';
 import clickFetch from '../../assets/undraw_click_fetch.png';
 import apiGallery from '../../assets/undraw_api_card_gallery.png';
-import Card from 'react-bootstrap/Card';
 
 const Docs = () => {
     return (
         <section className="docs">
-            <Card>
-                <Card.Body className='docStep card-body'>
-                    <p><strong>Step 1</strong>: Pick an API from the dropdown menu!</p>
-                    <img src={optionPicker} alt="Undraw option picker" />
-                </Card.Body>
-            </Card>
+            <div className='docStep'>
+                <p><strong>Step 1</strong>: Pick an API from the dropdown menu!</p>
+                <img src={optionPicker} alt="Undraw option picker" />
+            </div>
 
-            <Card>
-                <Card.Body className='docStep card-body'>
-                    <p><strong>Step 2</strong>: Hit the "Fetch!" button</p>
-                    <img src={clickFetch} alt="Undraw hit fetch button" />
-                </Card.Body>
-            </Card>
+            <div className='docStep'>
+                <p><strong>Step 2</strong>: Hit the "Fetch!" button</p>
+                <img src={clickFetch} alt="Undraw hit fetch button" />
+            </div>
 
-            <Card>
-                <Card.Body className='docStep card-body'>
-                    <p><strong>Step 3</strong>: View a row of cards that display data from the chosen API!</p>
-                    <img src={apiGallery} alt="Undraw API card gallery" />
-                </Card.Body>
-            </Card>
-
+            <div className='docStep'>
+                <p><strong>Step 3</strong>: See the API in action!</p>
+                <img src={apiGallery} alt="Undraw API card gallery" />
+            </div>
         </section>
     )
 }
